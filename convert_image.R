c75165f1caf3cc861786bc228448de3f69a1222d
Chitran1987 <ghosal.chitran@gmail.com>
2026-02-11 23:46:35 +0100

    Updated the convert file

--- convert_image.R
+++ convert_image.R
@@ -10,3 +10,7 @@ save(tens, file = 'tens.rda')
 #2nd Image
 tens_2 <- img_2_arr(source.png = 'm1_Z TraceUp Thu Oct 26 16.54.59 2023 [25-1]  STM_Spectroscopy STM.png', x.lim = c(0, 6.817), y.lim = c(0, 6.817))
 save(tens_2, file = 'tens_2.rda')
+
+#3rd Image
+tens_3 <- img_2_arr(source.png = 'm1_tensor Fri Aug 18 15.34.36 2023 [13-2]  STM_Spectroscopy STM_cut.png', x.lim = c(0, 19.077), y.lim = c(0, 11.543))
+save(tens_3, file = 'tens_3.rda')
